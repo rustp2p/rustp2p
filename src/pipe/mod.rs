a7b83b56a070188cac68d2ec35949dae5216d373
lbl <1791778603@qq.com>
2024-09-04 10:33:54 +0800

    handle Protocol

--- src/pipe/mod.rs
+++ src/pipe/mod.rs
@@ -3,6 +3,8 @@ use crate::protocol::node_id::NodeID;
 use crate::protocol::protocol_type::ProtocolType;
 use crate::protocol::NetPacket;
 use rust_p2p_core::route::RouteKey;
+use std::io;
+use std::net::SocketAddr;
 
 pub struct Pipe {
     pipe: rust_p2p_core::pipe::Pipe<NodeID>,
@@ -32,10 +34,7 @@ pub struct PipeLine {
     pipe_line: rust_p2p_core::pipe::PipeLine,
 }
 impl PipeLine {
-    pub async fn recv_from<'a>(
-        &mut self,
-        buf: &'a mut [u8],
-    ) -> Option<std::io::Result<RecvResult<'a>>> {
+    pub async fn recv_from<'a>(&mut self, buf: &'a mut [u8]) -> Option<io::Result<RecvResult<'a>>> {
         let (len, route_key) = match self.pipe_line.recv_from(buf).await? {
             Ok((len, route_key)) => (len, route_key),
             Err(e) => return Some(Err(e)),
@@ -45,6 +44,22 @@ impl PipeLine {
             Err(e) => return Some(Err(e)),
         };
         Some(Ok(RecvResult { packet, route_key }))
+    }
+    pub async fn handle(&mut self, recv_result: RecvResult<'_>) {
+        let packet = recv_result.packet;
+        let route_key = recv_result.route_key;
+        match packet.protocol() {
+            ProtocolType::PunchConsult => {}
+            ProtocolType::PunchRequest => {}
+            ProtocolType::PunchReply => {}
+            ProtocolType::EchoRequest => {}
+            ProtocolType::EchoReply => {}
+            ProtocolType::TimestampRequest => {}
+            ProtocolType::TimestampReply => {}
+            ProtocolType::IDRouteQuery => {}
+            ProtocolType::IDRouteReply => {}
+            _ => {}
+        }
     }
 }
 
@@ -63,5 +78,14 @@ impl<'a> RecvResult<'a> {
         } else {
             None
         }
+    }
+    pub fn src_id(&self) -> io::Result<NodeID> {
+        NodeID::new(self.packet.src_id())
+    }
+    pub fn dest_id(&self) -> io::Result<NodeID> {
+        NodeID::new(self.packet.dest_id())
+    }
+    pub fn remote_addr(&self) -> SocketAddr {
+        self.route_key.addr()
     }
 }
