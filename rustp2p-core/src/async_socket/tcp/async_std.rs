a3509e860d298beccee7a4306c455df4fe5c884e
lbl <1791778603@qq.com>
2024-11-01 19:10:08 +0800

    update

--- rustp2p-core/src/async_socket/tcp/async_std.rs
+++ rustp2p-core/src/async_socket/tcp/async_std.rs
@@ -1,1 +1,22 @@
+use std::io;
+use std::ops::Deref;
+use async_io::Async;
 
+#[repr(transparent)]
+pub struct TcpStream {
+    inner: Async<std::net::TcpStream>,
+}
+
+impl Deref for TcpStream {
+    type Target = Async<std::net::TcpStream>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl TcpStream {
+    pub async fn writable(&self) -> io::Result<()> {
+        self.inner.writable().await
+    }
+}
--- rustp2p-core/src/async_socket/udp/async_std.rs
+++ rustp2p-core/src/async_socket/udp/async_std.rs
@@ -1,27 +1,31 @@
 use std::io;
 use std::net::SocketAddr;
+use std::ops::Deref;
+use async_io::Async;
 
+#[repr(transparent)]
 pub struct UdpSocket {
-    inner: async_io::Async<std::net::UdpSocket>,
+    inner: Async<std::net::UdpSocket>,
+}
+impl Deref for UdpSocket {
+    type Target = Async<std::net::UdpSocket>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
 }
 
 impl UdpSocket {
     pub fn from_std(udp: std::net::UdpSocket) -> io::Result<Self> {
         Ok(Self {
-            inner: async_io::Async::new(udp)?,
+            inner: Async::new(udp)?,
         })
     }
 
     pub fn try_send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
         self.inner.get_ref().send_to(buf, addr)
     }
-    pub async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
-        self.inner.send_to(buf, addr).await
-    }
 
-    pub async fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        self.inner.recv_from(buf).await
-    }
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
         self.inner.get_ref().local_addr()
     }
